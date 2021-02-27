4be023aad42b5e93e444d2cbc03ab2e03b1a0e5b
nara-ryoya <nara_ryoya@icloud.com>
2021-02-27 16:42:29 +0900

    完了

--- app.py
+++ app.py
@@ -13,11 +13,13 @@ def route(path, method):
     routes.append((path, method))
 
 route('/', 'index')
-route('/next', 'next')
 route('/index', 'index')
-route('/xml', 'xml')
 
-
+data = [
+    ['それは人間のペットですか', 1, 2],
+    ['それはにゃーと鳴きますか', 'ネコ', "イヌ" ],
+    ["それは食べるとおいしいですか", "うし", "ライオン"]
+]
 
 
 class HelloServerHandler(BaseHTTPRequestHandler):
@@ -31,71 +33,116 @@ class HelloServerHandler(BaseHTTPRequestHandler):
         else:
             self.error()
         return
-
+    
     def do_POST(self):
+        _url = urlparse(self.path)
+        if _url.path == "/":
+            self.quiz()
+        elif _url.path == "/end":
+            self.end()
+        return
+
+
+    def error(self):
+        self.send_error(404, "cannot access!")
+        return
+
+    def quiz(self):
         form = FieldStorage(
             fp = self.rfile,
             headers=self.headers,
             environ={'REQUEST_METHOD': 'POST'}
         )
-        if 'sel1' in form:
-            items = str(form.getlist('sel1'))
-        else:
-            items = "no-item"
-        res = f"your OS: {items}"
+        
+        try:
+            answer = int(form["answer"].value)
+            if  answer == -1:
+                html = index.format(
+                    title="Animal",
+                    message="やったー！",
+                    last=-1,
+                    animal="",
+                    yes = 0,
+                    no = 0
+                )
+            elif answer == -2:
+                html = next.format(
+                    title='Animal',
+                    message='うーん、答えを教えて。',
+                    last=form['last'].value,
+                    animal=form['animal'].value
+                )
+            else:
+                html = index.format(
+                    title='Animal',
+                    message=data[answer][0],
+                    last = answer,
+                    animal=form['answer'].value,
+                    yes=data[answer][1],
+                    no=data[answer][2]
+                )
+        except:
+            html=index.format(
+                title='Animal',
+                message = f'それは、{form["answer"].value}ですか。',
+                last=form['last'].value,
+                animal=form['answer'].value,
+                yes = -1,
+                no = -2,
+            )
         self.send_response(200)
         self.end_headers()
-        html = next.format(
-            message = res,
-            data = form
-        )
         self.wfile.write(html.encode('utf-8'))
         return
 
     def index(self):
-        _url = urlparse(self.path)
         self.send_response(200)
         self.end_headers()
         html = index.format(
             title='Hello',
-            message='ようこそ、HTTPServerの世界へ！'
-        )
-        self.wfile.write(html.encode('utf-8'))
-        return
-    
-    def next(self):
-        _url = urlparse(self.path)
-        self.send_response(200)
-        self.end_headers()
-        html = next.format(
-            message = msg,
-            data = query,
-            header = self.headers
+            message='質問に答えて！',
+            last=-1,
+            animal="",
+            yes=0,
+            no=0
         )
         self.wfile.write(html.encode('utf-8'))
         return
 
-    def error(self):
-        self.send_header(404, "cannot access!")
-        return
-    
-    def xml(self):
-        xml =  '''<?xml version="1.0" encoding="UTF-8"?>
-        <data>
-            <person>
-                <name>Nara Ryoya</name>
-                <mail>nara_ryoya@icloud.com</mail>
-                <age>39</age>
-            </person>
-            <message>Hello Python!</message>
-        </data>
-        '''
+    def end(self):
+        form=FieldStorage(
+            fp=self.rfile,
+            headers=self.headers,
+            environ={'REQUEST_METHOD': 'POST'}
+        )
+        animalname = form['animalname'].value
+        question = form['question'].value
+        last = int(form['last'].value)
+        animal = form['animal'].value
+        newdata = [question, animalname, animal]
+        n = len(data)
+        if data[last][1] == animal:
+            data[last][1] = n
+        elif data[last][2] == animal:
+            data[last][2] = n
+        data.append(newdata)
+
+        html = index.format(
+            title='Animal',
+            message='なるほど、分かりました',
+            last=-1,
+            animal='',
+            yes=0,
+            no=0
+        )
+
         self.send_response(200)
-        self.send_header('Content-Type', \
-            'text/plain; charset=utf-8')
         self.end_headers()
-        self.wfile.write(xml.encode('utf-8'))
+        self.wfile.write(html.encode('utf-8'))
         return
 
-server = HTTPServer(('', 5000), HelloServerHandler)
+
+
+
+server = HTTPServer(('', 8000), HelloServerHandler)
 server.serve_forever()
